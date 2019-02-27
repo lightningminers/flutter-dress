0014d98e8c311868df049ae9ed94ec4e5e1d7475
阮细浩 <rxh12352@gmail.com>
2019-02-28 00:18:59 +0800

    完成 issues 结构

--- lib/pages/issues_page.dart
+++ lib/pages/issues_page.dart
@@ -2,9 +2,44 @@ import 'package:flutter/material.dart';
 
 class IssuesPage extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
-    return new Center(
-      child: new Text('IssuesPage'),
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      padding: EdgeInsets.all(10.0),
+      child: Container(
+          foregroundDecoration:
+              BoxDecoration(border: Border.all(color: Colors.black12)),
+          child: MyTab()),
     );
+  }
+}
+
+class MyTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return DefaultTabController(
+        length: 2,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            TabBar(
+              labelColor: Colors.blue,
+              tabs: <Widget>[
+                Tab(
+                  text: 'open',
+                ),
+                Tab(
+                  text: 'closed',
+                )
+              ],
+            ),
+            Expanded(
+              child: TabBarView(
+                children: <Widget>[Text('data'), Text('sd')],
+              ),
+            )
+          ],
+        ));
   }
 }
